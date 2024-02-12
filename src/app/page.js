22a927eb02ddb079e2f9d6c04872b6f68df2a82d
Helio Sales Jr <heliosalesjr@gmail.com>
2024-02-12 18:33:41 -0300

    1st commit: bento header made with css grid + tailwind

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Bento from "./components/Bento";
+import { Divider } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    
+    <>
+      <Bento />
+      <Divider className="mx-auto max-w-5xl my-8"/>
+      
+    </>
+  );
+}
